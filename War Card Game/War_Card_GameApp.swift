6a0183382a2e9d23e388f3762ea113189e5c9640
hafied <hafied.shiddiqi@ist.id>
2024-09-02 11:09:51 +0700

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by hafied on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
